bc9db1b36ff737ddcb97379834c36e42a7803bc5
Elisa Chen <skechen@gmail.com>
2020-11-23 22:42:33 +0100

    menu-items.js and questions-answers.js done and minor changes made to index.html and styles.css

--- menu-items.js
+++ menu-items.js
@@ -1,17 +1,359 @@
-// create a class MenuItems
-// properties include:
-    // name : name of menu item
-    // category: app, main, sweets
-    // price: number
-    // containsFish: true/false
-    // containsCrustacean: true/false
-    // containsCheese: true/false
-    // containsLactose: true/false
-    // glutenFree: true/false
-    // vegetarian: true/false
-    // containsAlcohol: 
-    // containsRaw: true/false
-    // hallal/kosher: true/false
-    // richness: number from 1-5
+class MenuItem {
+    constructor(item) {
+        this.name = item.name; // name of menu item
+        this.category = item.category; // app, main, sweets
+        this.price = item.price; // numerical price without dollar sign
+        this.containsFish = item.containsFish; //true or false
+        this.containsCrustacean = item.containsCrustacean; //true or false
+        this.containsCheese = item.containsCheese; //true or false
+        this.containsLactose = item.containsLactose; //true or false
+        this.glutenFree = item.glutenFree; //true or false
+        this.vegetarian = item.vegetarian; //true or false
+        this.containsAlcohol = item.containsAlcohol; //true or false
+        this.containsRaw = item.containsRaw; //true or false
+        this.halal = item.halal; //true or false
+        this.kosher = item.kosher; //true or false
+        this.richness = item.richness // 1 = very light, 5 = very rich
+    }
+}
 
-// create all the menu items from the figma
+const ricottaCarrots = new MenuItem({
+    name: "Smoked ricotta with roasted carrots",
+    category: 'APP',
+    price: 7,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: true,
+    containsLactose: true,  
+    glutenFree: true,
+    vegetarian: true,
+    containsAlcohol: false,
+    containsRaw: false,
+    halal: true,
+    kosher: true,
+    richness: 2,   
+});
+
+let pateEnCroute = new MenuItem({
+    name: 'Paté en croute',
+    category: 'APP',
+    price: 12,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: false,
+    glutenFree: false,
+    vegetarian: false,
+    containsAlcohol: true,
+    containsRaw: true,
+    halal: false,
+    kosher: false,
+    richness: 4    
+})
+
+let eggplantCaviar = new MenuItem({
+    name: 'Eggplant caviar',
+    category: 'APP',
+    price: 7,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: false,
+    glutenFree: true,
+    vegetarian: true,
+    containsAlcohol: false,
+    containsRaw: false,
+    halal: true,
+    kosher: true,
+    richness: 1    
+})
+
+let baconScone = new MenuItem({
+    name: 'Bacon scone with maple syrup and clotted cream',
+    category: 'APP',
+    price: 9,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: true,
+    glutenFree: false,
+    vegetarian: false,
+    containsAlcohol: true,
+    containsRaw: true,
+    halal: false,
+    kosher: false,
+    richness: 4    
+})
+
+let caesarSalad = new MenuItem({
+    name: 'Caesar salad',
+    category: 'APP',
+    price: 9,
+    containsFish: true,
+    containsCrustacean: false,
+    containsCheese: true,
+    containsLactose: true,
+    glutenFree: true,
+    vegetarian: false,
+    containsAlcohol: false,
+    containsRaw: true,
+    halal: false,
+    kosher: false,
+    richness: 3    
+})
+
+let vitelloTonnato = new MenuItem({
+    name: 'Vitello tonnato',
+    category: 'APP',
+    price: 11,
+    containsFish: true,
+    containsCrustacean: false,
+    containsCheese: true,
+    containsLactose: true,
+    glutenFree: true,
+    vegetarian: false,
+    containsAlcohol: false,
+    containsRaw: false,
+    halal: true,
+    kosher: false,
+    richness: 4    
+})
+
+let chickenTagine = new MenuItem({
+    name: 'Chicken Tagine with Bulgour',
+    category: 'MAIN',
+    price: 19,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: false,
+    glutenFree: false,
+    vegetarian: false,
+    containsAlcohol: true,
+    containsRaw: true,
+    halal: true,
+    kosher: true,
+    richness: 3    
+})
+
+let blanquetteVeau = new MenuItem({
+    name: 'Blanquette de veau',
+    category: 'MAIN',
+    price: 21,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: true,
+    glutenFree: true,
+    vegetarian: false,
+    containsAlcohol: true,
+    containsRaw: false,
+    halal: true,
+    kosher: false,
+    richness: 4    
+})
+
+let fennelGratin = new MenuItem({
+    name: 'Fennel gratin with olives and confit lemon',
+    category: 'MAIN',
+    price: 16,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: true,
+    glutenFree: false,
+    vegetarian: true,
+    containsAlcohol: false,
+    containsRaw: false,
+    halal: true,
+    kosher: true,
+    richness: 3    
+})
+
+let thaiGreenCurry = new MenuItem({
+    name: 'Thai green curry',
+    category: 'MAIN',
+    price: 17,
+    containsFish: true,
+    containsCrustacean: true,
+    containsCheese: false,
+    containsLactose: false,
+    glutenFree: true,
+    vegetarian: true,
+    containsAlcohol: false,
+    containsRaw: false,
+    halal: true,
+    kosher: true,
+    richness: 2    
+})
+
+let scallopsLeeks = new MenuItem({
+    name: 'Scallops with buttered leeks',
+    category: 'MAIN',
+    price: 28,
+    containsFish: false,
+    containsCrustacean: true,
+    containsCheese: false,
+    containsLactose: true,
+    glutenFree: true,
+    vegetarian: false,
+    containsAlcohol: true,
+    containsRaw: false,
+    halal: true,
+    kosher: false,
+    richness: 2    
+})
+
+let pithiviers = new MenuItem({
+    name: 'Pithiviers',
+    category: 'MAIN',
+    price: 27,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: false,
+    glutenFree: false,
+    vegetarian: false,
+    containsAlcohol: true,
+    containsRaw: true,
+    halal: false,
+    kosher: false,
+    richness: 5    
+})
+
+let pumpkinTortellini = new MenuItem({
+    name: 'Pumpkin and Ricotta Tortellini',
+    category: 'MAIN',
+    price: 18,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: true,
+    containsLactose: true,
+    glutenFree: false,
+    vegetarian: true,
+    containsAlcohol: false,
+    containsRaw: true,
+    halal: true,
+    kosher: true,
+    richness: 3    
+})
+
+let chocolateCake = new MenuItem({
+    name: 'Chocolate cake with vanilla bourbon ice cream',
+    category: 'SWEET',
+    price: 8,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: true,
+    glutenFree: false,
+    vegetarian: true,
+    containsAlcohol: true,
+    containsRaw: false,
+    halal: false,
+    kosher: true,
+    richness: 4    
+})
+
+let babaAuRhum = new MenuItem({
+    name: 'Baba au rhum',
+    category: 'SWEET',
+    price: 10,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: true,
+    glutenFree: false,
+    vegetarian: true,
+    containsAlcohol: true,
+    containsRaw: false,
+    halal: false,
+    kosher: true,
+    richness: 4    
+})
+
+let tiramisu = new MenuItem({
+    name: 'Tiramisu',
+    category: 'SWEET',
+    price: 8,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: true,
+    glutenFree: false,
+    vegetarian: true,
+    containsAlcohol: true,
+    containsRaw: false,
+    halal: false,
+    kosher: true,
+    richness: 4    
+})
+
+let chocChipCookies = new MenuItem({
+    name: 'Chocolate chip cookies',
+    category: 'SWEET',
+    price: 6,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: false,
+    glutenFree: true,
+    vegetarian: true,
+    containsAlcohol: false,
+    containsRaw: false,
+    halal: true,
+    kosher: true,
+    richness: 3    
+})
+
+let poachedPears = new MenuItem({
+    name: 'Poached pears',
+    category: 'SWEET',
+    price: 9,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: false,
+    glutenFree: true,
+    vegetarian: true,
+    containsAlcohol: true,
+    containsRaw: false,
+    halal: false,
+    kosher: true,
+    richness: 1    
+})
+
+let pineappleCarpaccio = new MenuItem({
+    name: 'Pineapple Carpaccio',
+    category: 'SWEET',
+    price: 7,
+    containsFish: false,
+    containsCrustacean: false,
+    containsCheese: false,
+    containsLactose: false,
+    glutenFree: true,
+    vegetarian: true,
+    containsAlcohol: false,
+    containsRaw: false,
+    halal: true,
+    kosher: true,
+    richness: 1    
+})
+
+// template for new items
+// let newItem = new MenuItem({
+//     name: '',
+//     category: '',
+//     price: 0,
+//     containsFish: true,
+//     containsCrustacean: true,
+//     containsCheese: true,
+//     containsLactose: true,
+//     glutenFree: true,
+//     vegetarian: true,
+//     containsAlcohol: true,
+//     containsRaw: true,
+//     halal: true,
+//     kosher: true,
+//     richness: 1    
+// })
--- questions-answers.js
+++ questions-answers.js
@@ -1,5 +1,82 @@
 // createa a class Questions
 
-// properties include: 
-    // question
-    // rightAnswer
+class Prompt {
+    constructor (info) {
+        this.question = info.question;
+        this.wrongAnswer = info.wrongAnswer;
+        this.rightAnswer = info.rightAnswer;
+    }
+}
+
+let vegetarian = new Prompt ({
+    question: "I'm vegetarian and don't eat any meat or fish. What can you recommend?",
+    wrongAnswer: "I told you I don't eat our animal or fish friends! I'm outta here!",
+    rightAnswer: "Perfect! This sounds delicious and virtuous."
+});
+
+let fishAllergy = new Prompt ({
+    question: "I'm deathly allergic to fish, my throat will swell shut. What can I have?",
+    wrongAnswer: 'Jesus, are you trying to kill me?!',
+    rightAnswer: "Thank you for taking my allergy seriously, that sounds great!"
+});
+
+let crustaceanAllergy = new Prompt ({
+    question: "I can't eat any shellfish. Can you recommend somethign for me?",
+    wrongAnswer: "No way, that's going to send me to the hospital!",
+    rightAnswer: "Wonderful, that should work for me!"
+});
+
+let noCheese = new Prompt ({
+    question: 'I hate cheese. Can you suggest a menu without any cheese at all?',
+    wrongAnswer: "Ugh, I know there's some parmesan hanging out in there!",
+    rightAnswer: "Perfect, not a shred of cheese anywhere!"
+});
+
+let lactoseIntolerant = new Prompt ({
+    question: "I'm lactose intolerant, even a drop is too much. What can I have?",
+    wrongAnswer: "You know that's going to make me feel bloated all night!",
+    rightAnswer: "Great, that sounds digestively perfect!"
+});
+
+let halal = new Prompt ({
+    question: 'I eat strictly halal. What can I have here?',
+    wrongAnswer: "You're disrespecting my religious beliefs! I can't eat that!",
+    rightAnswer: "That sounds wonderful, thank you"
+});
+
+let kosher = new Prompt ({
+    question: "I each strictly kosher. Do you know what I can eat?",
+    wrongAnswer: "No way! That is so NOT kosher.",
+    rightAnswer: "I'll go with your suggestions, thanks!"
+});
+
+let glutenFree = new Prompt ({
+    question: "My body can't process anything with gluten. Help me!",
+    wrongAnswer: "That's going to make my stomach explode! ",
+    rightAnswer: "Ooh, that sounds lovely. "
+});
+
+let newPrompt = new Prompt ({
+    question: "I'm trying to eat lighter. What would you recommend?",
+    wrongAnswer: "Did you not hear me when I said 'lighter'? Not 'richer'! ",
+    rightAnswer: "That sounds like the perfect meal for my diet!"
+});
+
+let noAlcohol = new Prompt ({
+    question: "I've sworn off alcohol for dry January. What can I have?",
+    wrongAnswer: "Uhhh, I know there's some booze hiding in there!",
+    rightAnswer: "Sweet, thanks for helping me stick to my resolutions!"
+});
+
+let pregnant = new Prompt ({
+    question: "I'm pregnant and can't eat any raw or uncooked foods. What do you suggest?",
+    wrongAnswer: "Are you trying to give me listeriosis?",
+    rightAnswer: "Thanks for helping me navigating these restrictions!"
+});
+
+// template for new prompts 
+// let newPrompt = new Prompt ({
+//     question: '',
+//     wrongAnswer: '',
+//     rightAnswer: ''
+// });
